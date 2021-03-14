6c69ac5f94d505720d959500994a0a71d12c4941
Roman Russkikh <r-ru@yandex.ru>
2021-03-14 17:36:45 +0300

    Добавил сущности Delivery и Shop

---
+++ src/app/model/Delivery.ts
@@ -0,0 +1,38 @@
+
+export class Delivery {
+  id: number;
+  deliveryDate: Date;    // только дата, без времени
+  deliveryTime: string;  // по факту - Enum
+  carInfo: string;
+  driverInfo: string;
+  brand: string;
+  orderNumber: string;
+  deliveryType: string;    // по факту - Enum
+  sender: string;
+  comment: string;
+  shop: string;
+  numberOfPlaces: string;
+  torgNumber: string;
+  invoice: string;
+  user: string;
+
+
+  constructor(id: number, deliveryDate: Date, deliveryTime: string, carInfo: string, driverInfo: string, brand: string, orderNumber: string, deliveryType: string, sender: string, comment: string, shop: string, numberOfPlaces: string, torgNumber: string, invoice: string, user: string) {
+    this.id = id;
+    this.deliveryDate = deliveryDate;
+    this.deliveryTime = deliveryTime;
+    this.carInfo = carInfo;
+    this.driverInfo = driverInfo;
+    this.brand = brand;
+    this.orderNumber = orderNumber;
+    this.deliveryType = deliveryType;
+    this.sender = sender;
+    this.comment = comment;
+    this.shop = shop;
+    this.numberOfPlaces = numberOfPlaces;
+    this.torgNumber = torgNumber;
+    this.invoice = invoice;
+    this.user = user;
+  }
+}
+}
---
+++ src/app/model/Shop.ts
@@ -0,0 +1,10 @@
+import {Brand} from './Brand';
+
+export class Shop {
+
+  id: number;
+  name: string;
+  abbr: string;
+  brand: Brand;
+
+}
