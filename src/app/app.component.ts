8c9d916561b8aa09c98e02b850d4519ecaab646e
Roman Russkikh <r-ru@yandex.ru>
2021-03-16 12:08:02 +0300

    Удалил лишний код сайдбара

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,7 @@ export class AppComponent {
   title = 'warehouse';
 
   // параметры бокового меню с категориями
-  _opened: boolean = false;
-  _modeNum: number = 0;
+  _opened: boolean = true;
   _positionNum: number = 0;
   _dock: boolean = false;
   _closeOnClickOutside: boolean = false;
@@ -28,62 +27,6 @@ export class AppComponent {
 
   _toggleOpened(): void {
     this._opened = !this._opened;
-  }
-
-  _toggleMode(): void {
-    this._modeNum++;
-
-    if (this._modeNum === this._MODES.length) {
-      this._modeNum = 0;
-    }
-  }
-
-  _toggleAutoCollapseHeight(): void {
-    this._autoCollapseHeight = this._autoCollapseHeight ? null : 500;
-  }
-
-  _toggleAutoCollapseWidth(): void {
-    this._autoCollapseWidth = this._autoCollapseWidth ? null : 500;
-  }
-
-  _togglePosition(): void {
-    this._positionNum++;
-
-    if (this._positionNum === this._POSITIONS.length) {
-      this._positionNum = 0;
-    }
-  }
-
-  _toggleDock(): void {
-    this._dock = !this._dock;
-  }
-
-  _toggleCloseOnClickOutside(): void {
-    this._closeOnClickOutside = !this._closeOnClickOutside;
-  }
-
-  _toggleCloseOnClickBackdrop(): void {
-    this._closeOnClickBackdrop = !this._closeOnClickBackdrop;
-  }
-
-  _toggleShowBackdrop(): void {
-    this._showBackdrop = !this._showBackdrop;
-  }
-
-  _toggleAnimate(): void {
-    this._animate = !this._animate;
-  }
-
-  _toggleTrapFocus(): void {
-    this._trapFocus = !this._trapFocus;
-  }
-
-  _toggleAutoFocus(): void {
-    this._autoFocus = !this._autoFocus;
-  }
-
-  _toggleKeyClose(): void {
-    this._keyClose = !this._keyClose;
   }
 
   _onOpenStart(): void {
