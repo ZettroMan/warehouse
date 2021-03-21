41b559414c9886e8e4fd2aba70c73405772bd847
Roman Russkikh <r-ru@yandex.ru>
2021-03-22 00:29:30 +0300

    Сделал кнопки в боковой панели

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,9 +4,11 @@ import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 const MaterialComponents = [
-  MatButtonModule, MatIconModule, MatTableModule, MatSidenavModule, MatToolbarModule
+  MatButtonModule, MatIconModule, MatTableModule, MatSidenavModule,
+  MatToolbarModule, MatListModule
 ];
 
 @NgModule({
