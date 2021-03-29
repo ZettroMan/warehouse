4e2e0658ecd4032ffd9990b1fc350d978989e2fe
Explosionman <rybinskov@gmail.com>
2021-03-29 22:36:21 +0300

    исправил баги с меню

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,11 @@ import {TokenStorageService} from './views/security/auth/token-storage.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   roles: string[];
   authorized = false;
+  admin = false;
+  warehouse = false;
   // тип устройства - на будущее
   isMobile: boolean;
   isTablet: boolean;
@@ -20,15 +22,36 @@ export class AppComponent  implements OnInit{
   // параметры бокового меню с категориями
   menuOpened = true;  // по умолчанию - открыто
 
-  constructor(private tokenStorage: TokenStorageService) { }
+  constructor(private tokenStorage: TokenStorageService) {
+  }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
       this.roles = this.tokenStorage.getAuthorities();
-      this.authorized = true;
     } else {
       this.authorized = false;
     }
+  }
+
+  getUserRole(): string {
+    this.roles = this.tokenStorage.getAuthorities();
+    let role: string;
+    for (let i = 0; i < this.roles.length; i++) {
+      role = JSON.stringify(this.roles[i]).split('":"')[1];
+      role = role.substring(0, role.length - 2);
+      if (role.startsWith('ROLE')) {
+        return role;
+      }
+    }
+    return null;
+  }
+
+  isAdmin(): boolean {
+    return this.getUserRole() === 'ROLE_ADMIN';
+  }
+
+  isWarehouse(): boolean {
+    return this.getUserRole() === 'ROLE_WAREHOUSE';
   }
 
   // TODO - здесь надо будет потом вставить SearchValues для тех сущностей, по которым нужно осуществлять поиск
